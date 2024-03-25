30cdaba1fed6adc6b7686a62019d4f05ce967626
Dovile <dovile.maminskaite@am.lt>
2024-03-25 13:14:27 +0200

    remove react query dev tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { MutationCache, QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AxiosError } from 'axios';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -71,6 +70,5 @@ root.render(
         </UserProvider>
       </BrowserRouter>
     </ThemeProvider>
-    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>,
 );
